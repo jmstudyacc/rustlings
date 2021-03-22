c7abd51f4fb0fc8fff9ace39654aeac5ad7d45f8
jmstudyacc <jmstudyacc@gmail.com>
2021-03-22 22:15:45 +0000

    Day 1 of 100 days of code

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,13 +1,11 @@
 // functions5.rs
 // Make me compile! Execute `rustlings hint functions5` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The answer is {}", answer);
 }
 
-fn square(num: i32) -> i32 {
-    num * num;
+fn square(mut num: i32) -> i32 {
+    num * &num         // remove the return statement and allow it to just return the value
 }
